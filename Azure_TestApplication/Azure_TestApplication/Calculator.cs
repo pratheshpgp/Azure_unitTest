8f0fecc30f06cb710722fd8fb0e5a5018c1c7d5e
pratheshpgp <45036226+pratheshpgp@users.noreply.github.com>
2020-11-05 16:42:40 +0530

    Add files via upload

---
+++ Azure_TestApplication/Azure_TestApplication/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Azure_TestApplication
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Azure_TestApplication/Azure_TestApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Azure_TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+
+            int result = calculator.Add(5, 6);
+
+            if (result != 11)
+                throw new InvalidOperationException();
+        }
+    }
+}
